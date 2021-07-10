05194300f12669d3ea56b05c23e4d53b4de1d6d1
Marcos <marcos.mendonca@matera.com>
2021-07-10 17:19:35 -0300

    Revoke token

---
+++ backend/src/main/java/com/mvam/etcontact/controllers/TokenController.java
@@ -0,0 +1,31 @@
+package com.mvam.etcontact.controllers;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@RestController
+@RequestMapping("/tokens")
+public class TokenController {
+
+    @Value("${security.oauth2.client.enable-https}")
+    private boolean isEnableHttps;
+
+    @DeleteMapping("/revoke")
+    public void revoke(HttpServletRequest request, HttpServletResponse response) {
+        Cookie cookie = new Cookie("refreshToken", null);
+        cookie.setHttpOnly(true);
+        cookie.setSecure(isEnableHttps); //se true, só irá aceitar HTTPS
+        cookie.setPath(request.getContextPath() + "/oauth/token");
+        cookie.setMaxAge(0);
+
+        response.addCookie(cookie);
+        response.setStatus(HttpStatus.NO_CONTENT.value());
+    }
+}

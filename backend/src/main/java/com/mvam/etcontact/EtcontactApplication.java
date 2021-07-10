28a84dcab21fe29d88caa8174c41c7b132c81a76
Marcos <marcos.mendonca@matera.com>
2021-07-10 14:59:26 -0300

    Backend first commit

---
+++ backend/src/main/java/com/mvam/etcontact/EtcontactApplication.java
@@ -0,0 +1,13 @@
+package com.mvam.etcontact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EtcontactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EtcontactApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/mvam/etcontact/EtcontactApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mvam.etcontact;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EtcontactApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

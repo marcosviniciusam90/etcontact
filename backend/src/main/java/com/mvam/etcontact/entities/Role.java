bf810aec0955a0b5a15df99bf157975aae403bf5
Marcos <marcos.mendonca@matera.com>
2021-07-10 16:41:34 -0300

    OAuth2 + JWT

---
+++ backend/src/main/java/com/mvam/etcontact/entities/Role.java
@@ -0,0 +1,23 @@
+package com.mvam.etcontact.entities;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+@Table(name = "tb_role")
+public class Role implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @EqualsAndHashCode.Include
+    private String authority;
+}
